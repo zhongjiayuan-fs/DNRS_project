e173ed4881ddee8c65d0856aaf241d7bd8d30e44
zhongjiayuan <51307526+zhongjiayuan@users.noreply.github.com>
2022-03-29 10:08:00 +0800

    Update NRS_curve.m

--- NRS_curve.m
+++ NRS_curve.m
@@ -5,11 +5,11 @@
 clear;
 clc;
 close all;
-[hESCs_to_neurons_NRS_matrix,genes] = xlsread('hESCs_to_neurons_NRS_matrix.xlsx');
-hESCs_to_neurons_NRS_matrix_size=size(hESCs_to_neurons_NRS_matrix);
+[hESCs_to_neurons_DNRS_matrix,genes] = xlsread('hESCs_to_neurons_DNRS_matrix.xlsx');
+hESCs_to_neurons_DNRS_matrix_size=size(hESCs_to_neurons_DNRS_matrix);
 for t=1:5
-    [sort_result_curve,idx]=sort(hESCs_to_neurons_NRS_matrix(:,t),'descend');
-    tmp=sort_result_curve(1:hESCs_to_neurons_NRS_matrix_size*0.05);
+    [sort_result_curve,idx]=sort(hESCs_to_neurons_DNRS_matrix(:,t),'descend');
+    tmp=sort_result_curve(1:196);
     case_result(t)=mean(tmp); 
 end
 plot(1:5,case_result,'r-*','LineWidth',3);
